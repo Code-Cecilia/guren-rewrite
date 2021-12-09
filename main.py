21e774b7c844cb6e8560d6153d33705dea30f80d
Yuichiro <Yuichiro>
2021-12-09 12:43:15 +0000

    update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import asyncio # Used for loop tasks
-import json # Used for json databases and data storing
-import os # Reads majority of files
-import random
+import asyncio  # Used for loop tasks
+import json  # Used for json databases and data storing
+import os  # Reads majority of files
 
 from pathlib import Path
-import discord # Using Pycord
+import discord  # Using Pycord
 from discord.ext import commands
+from discord.ext.commands.context import Context
 
 ####### Used Flags ########
-intents = discord.Intents
+intents = discord.Intents()
 intents.messages = True
 intents.bans = True
 intents.guilds = True
@@ -19,6 +19,10 @@ intents.members = True
 intents.voice_states = True
 ############################
 
+cwd = Path(__file__).parents[0]
+cwd = str(cwd)
+print(f"{cwd}\n-----")
+
 with open("./bot_files/settings/settings.json", 'r') as configFile: # Points to the settings.json file and calls it configFile in the code.
     data = json.load(configFile)
     token = data.get("token") # Token Data
@@ -28,13 +32,8 @@ with open("./bot_files/configs/status.json", 'r') as statusFile: # I decided to 
     data = json.load(statusFile)
     statuses = data.get("statuses")
 
-cwd = Path(__file__).parents[0]
-cwd = str(cwd)
-print(f"{cwd}\n-----")
-
 # I call my bot instance as bot, which means i will have to use bot everytime i want to mention it or create a command, etc. Thats its name.
-
-bot = commands.bot(
+bot = commands.Bot(
     command_prefix='$', # Temporary prefix
     owner_id=owner, # Grabs the owner id from the settings.json
     case_insensitive=True, # Allows upper case and lower case letters in commands, easier for mobile users with some keyboards.
@@ -42,26 +41,45 @@ bot = commands.bot(
 )
 
 bot.token = token
-bot.cwd = cwd
 bot.version = "0.1"
+bot.cwd = cwd
 
 @bot.event # Login event.
 async def on_ready():
+    print("\n----------------------")
     print('Logged in as', bot.user.name) # Prints the username of the bot.
     print('Bot ID:', bot.user.id) # Prints the user ID of the bot.
-    print('Bot latency:', bot.latency * 1000, 2) # Shows the bot latency to the API in the console.
+    print('Bot latency:', round(bot.latency * 1000, 2)) # Shows the bot latency to the API in the console.
     print('Running discord.py version ' + discord.__version__) # Shows the current library version.
+    print("\n----------------------")
 
 async def presence():
     await bot.wait_until_ready()
     while not bot.is_closed():
-        status = random.choice(statuses)    
-        await bot.change_presence(activity=discord.Game(status))
+        game = discord.Game(statuses)
+
+        await bot.change_presence(status=discord.Status.dnd, activity=game)
         await asyncio.sleep(30)
 
-for file in os.listdir(cwd = "/cogs"): # Searches files in the folder cogs.
-    if file.endswith(".py") and not file.startswith("_"): # Verifies that the file ends with .py and does not start with _ so it wont be loaded as a cog.
-        bot.load_extension(f"cogs.{file[:-3]}")
-    bot.loop.create_task(presence())
-    bot.run(bot.token)
-    bot.load_extension("jishaku")
+
+# Status that can be set:
+#
+# online
+# offline
+# idle
+# dnd
+# invisible
+# streaming
+# do_not_disturb - alias for dnd
+
+if __name__ == "__main__":
+    for file in os.listdir(cwd + "/cogs"):
+        if file.endswith(".py") and not file.startswith("_"):
+            bot.load_extension(f"cogs.{file[:-3]}")
+
+###################################################
+
+bot.loop.create_task(presence())
+bot.run(bot.token) 
+
+# Will only work if they are not inside the cogs loading loop - reminder`
