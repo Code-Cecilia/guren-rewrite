916f05c25bd8d028995b8326c15af467dc492ef7
Yuichiro <Yuichiro>
2021-12-09 12:43:43 +0000

    jishaku cog

---
+++ cogs/jishaku.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+from jishaku.features.python import PythonFeature
+from jishaku.features.root_command import RootCommand
+
+class Jishaku(PythonFeature, RootCommand):
+    pass
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded\n----------------------")
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Jishaku(bot=bot))
