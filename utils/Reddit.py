35d39df4d775ce30debf34d1f97adf4bbc66896f
Yuichiro <Yuichiro>
2021-12-11 16:25:23 +0000

    Co-authored-by: Mahas1 <Mahas1@users.noreply.github.com>

---
+++ utils/Reddit.py
@@ -0,0 +1,42 @@
+import praw, random
+ 
+class reddit_login:
+    def __init__(self, **credentials):
+        self.username = credentials['r_username']
+        self.redirect_uri = credentials['r_redirect_uri']
+        self.client_secret = credentials['r_client_secret']
+        self.user_agent = credentials['r_user_agent']
+        self.client_id = credentials['r_client_id']
+
+        self.reddit = praw.Reddit(
+                            client_id=self.client_id,
+                            redirect_uri=self.redirect_uri,
+                            client_secret=self.client_secret,
+                            user_agent=self.user_agent,
+                            username=self.username,
+                            check_for_async=False
+                        )
+    def get_random_title(self, subreddit, limit=10):
+        subreddit = self.reddit.subreddit(subreddit)
+        x = subreddit.top(limit=limit)
+        title_list = []
+        for y in x:
+            title_list.append(str(y.title))
+        choice = random.choice(title_list)
+        return choice
+
+    def get_post_data(self, subreddit, limit=10):
+        subreddit = self.reddit.subreddit(subreddit)
+        sub_list = []
+        x = subreddit.hot(limit=limit)
+        for y in x:
+            sub_list.append(y)
+        choice = random.choice(sub_list)
+
+        return {
+            "author": choice.author,
+            "url": choice.url,
+            "subreddit": subreddit.name,
+            "like_ratio": choice.like_ratio,
+            "title": choice.title
+        }
